7624e09c88ed830d58f85933e3bafb690ec3b962
Lavanyah-23 <venkatalalithalavanya@gmail.com>
2022-03-07 12:19:35 +0100

    established and tested connection with DB

--- migrations/20220307101802-set-up-relations.js
+++ migrations/20220307101802-set-up-relations.js
@@ -2,20 +2,37 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   
+    await queryInterface.addColumn("story", "userId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      allowNull: false,
+    });
+
+    await queryInterface.addColumn("story", "categoryId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "category",
+        allowNull: false,
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+    });
+    await queryInterface.addColumn("users", "isArtist", {
+      type: Sequelize.BOOLEAN,
+      
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("artworks", "userId", Sequelize.INTEGER);
+    await queryInterface.removeColumn("bids", "artworkId", Sequelize.INTEGER);
+    await queryInterface.removeColumn("users", "isArtist", Sequelize.BOOLEAN);
+    
+  },
 };
